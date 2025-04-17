f25874b674980d1d424c406b131caf7e71bba934
Luis Rodrigues <luisr.silva@outlook.com>
2025-04-16 22:24:38 -0300

    feat: Insert function and auxiliary functions

--- src/btree.c
+++ src/btree.c
@@ -12,17 +12,47 @@ BTreeNode* createNode(int t, bool is_leaf) {
   node->num_keys = 0;
 
   for(int i = 0; i < 2 * t; i++) {
-    node->children[i] = NULL; 
+    node->children[i] = NULL;
   }
-  
+
   return node;
 }
 
 BTree* createTree(int t) {
   BTree* tree = (BTree*)malloc(sizeof(BTree));
+  tree->root = createNode(t, true);
   return tree;
 }
 
+void insertKey(BTree *root, int key) {
+
+}
+
+void splitChild(BTreeNode *node, int index) {
+  const int NODE_CAPACITY = node->capacity;
+
+  BTreeNode* newNode = createNode(NODE_CAPACITY, node->children[index]->is_leaf);
+  BTreeNode* children = node->children[index];
+
+  newNode->num_keys = NODE_CAPACITY - 1;
+  for(int i = 0; i <= newNode->num_keys; i++) {
+    newNode->keys[i] = children->keys[i + NODE_CAPACITY];
+
+    if(!children->is_leaf) {
+      for(int j = 0; j < NODE_CAPACITY; j++) {
+        newNode->children[j] = children->children[j + NODE_CAPACITY];
+      }
+    }
+  }
+
+}
+
+void insertNonFull(BTreeNode *node, int key) {
+
+}
+
 int main(void) {
+  BTree* tree = createTree(3);
+  printf("%d\n", tree->root->capacity);
   return 0;
 }
--- src/btree.h
+++ src/btree.h
@@ -13,8 +13,11 @@ typedef struct BTree {
   int capacity;
 } BTree;
 
-BTreeNode* createNode(int t, bool is_leaf);
-BTree* createTree(int t);
-void destroyNode(BTreeNode* n);
-void destroyTree(BTree* b);
+BTreeNode* createNode(int capacity, bool is_leaf);
+BTree* createTree(int capacity);
+void destroyNode(BTreeNode* node);
+void destroyTree(BTree* tree);
 BTreeNode* search(BTreeNode* root, int key);
+void insertKey(BTree* root, int key);
+void splitChild(BTreeNode* node, int index);
+void insertNonFull(BTreeNode* node, int key);
